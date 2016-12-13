b73a2a84c5c15f2cebfb6da3ba7ee81c990c69ee
Marco Damaceno <maadamaceno@gmail.com>
2016-12-13 00:25:11 -0200

    Colorização

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ var bookVM = new Vue({
             selectedBook: {}
         }
     },
-    mounted: function() {
+    mounted: function () {
         var _this = this
 
         Promise.resolve(_this.parseData())
     },
     methods: {
-        parseData: function() {
+        parseData: function () {
             var _this = this
-            var p1 = new Promise(function(resolve, reject) {
+            var p1 = new Promise(function (resolve, reject) {
                 _this.$http.get('./data/nvi.json').then((response) => {
                     resolve(JSON.parse(response.data))
                 }, (response) => {
@@ -38,12 +38,12 @@ var bookVM = new Vue({
                 })
             })
 
-            p1.then(function(data) {
+            p1.then(function (data) {
                 _this.books = data
             })
         },
 
-        getBook: function(index) {
+        getBook: function (index) {
             var _this = this
             var result = jsonata('$[' + parseInt(index) + '].chapters').evaluate(_this.books)
 
@@ -64,7 +64,7 @@ var bookVM = new Vue({
             }
         },
 
-        getText: function(book, chapter) {
+        getText: function (book, chapter) {
             var _this = this
             var result = jsonata('$[' + parseInt(book) + '].chapters[' + (parseInt(chapter) - 1) + ']').evaluate(_this.books)
 
@@ -75,7 +75,7 @@ var bookVM = new Vue({
             _this.book.chapterVerses = result[chapter]
         },
 
-        goToListBook: function() {
+        goToListBook: function () {
             var _this = this
 
             // Controllers
@@ -87,6 +87,35 @@ var bookVM = new Vue({
             _this.control.selectedBook = {}
 
             _this.parseData()
+        },
+
+        setColorBg: function (index) {
+            var _this = this
+            var index = parseInt(index)
+
+            if (index <= 4) {
+                return "bg-blue-400"
+            } else if (index <= 16) {
+                return "bg-green-600"
+            } else if (index <= 21) {
+                return "bg-pink-300"
+            } else if (index <= 26) {
+                return "bg-danger"
+            } else if (index <= 38) {
+                return "bg-brown-400"
+            } else if (index <= 42) {
+                return "bg-teal-600"
+            } else if (index <= 43) {
+                return "bg-violet"
+            } else if (index <= 56) {
+                return "bg-danger-400"
+            } else if (index <= 64) {
+                return "bg-indigo-300"
+            } else if (index <= 65) {
+                return "bg-orange-400"
+            } else {
+                return ""
+            }
         }
     }
 })
